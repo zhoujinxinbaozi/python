5c0e9097c85ea1120a42a591e95ad6ac26c41f06
zhoujinxinbaozi <17513158065@163.com>
2019-08-18 13:53:00 +0800

    ...

--- com/basic/函数.py
+++ com/basic/函数.py
@@ -5,11 +5,17 @@
 @file: 函数.py
 @time: 2019/04/{DAY}
 """
+import time
+
 if __name__ == "__main__":
     pass
 
 def printMe(str) :
     print(str)
+    i = 0
+    while(i < 100000) :
+        i = i + 1
 
-
-print("周金鑫")
+t1 = int(round(time.time() * 1000))
+printMe("周金鑫")
+print(int(round(time.time() * 1000)) - t1)
